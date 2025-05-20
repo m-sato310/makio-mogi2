fe7e845dc5aca57af85403336a7362acc082253b
m.sato <mst.nt.zsof.3.0@gmail.com>
2025-05-20 12:25:58 +0900

    マイグレーションファイルを作成

---
+++ src/database/migrations/2025_05_20_115841_create_correction_requests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCorrectionRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('correction_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attendance_id')->constrained('attendances')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('new_clock_in');
+            $table->string('new_clock_out');
+            $table->string('remarks',255);
+            $table->string('approval_status');
+            $table->foreignId('approved_by')->nullable()->constrained('admins')->onDelete('set null');
+            $table->timestamp('approved_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('correction_requests');
+    }
+}
