d870df14f6716c47662f133aa248454f11e072f7
m.sato <mst.nt.zsof.3.0@gmail.com>
2025-06-17 16:19:08 +0900

    管理者ログイン認証機能テストコードを実装

---
+++ src/tests/Feature/Auth/AdminLoginTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AdminLoginTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_admin_login_validation_error_when_email_is_empty()
+    {
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('adminpass123'),
+            'is_admin' => true,
+        ]);
+
+        $data = [
+            'email' => '',
+            'password' => 'adminpass123',
+        ];
+
+        $response = $this->post('/admin/login', $data);
+
+        $response->assertSessionHasErrors([
+            'email' => 'メールアドレスを入力してください'
+        ]);
+    }
+
+    public function test_admin_login_validation_error_when_password_is_empty()
+    {
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('adminpass123'),
+            'is_admin' => true,
+        ]);
+
+        $data = [
+            'email' => 'admin@example.com',
+            'password' => '',
+        ];
+
+        $response = $this->post('/admin/login', $data);
+
+        $response->assertSessionHasErrors([
+            'password' => 'パスワードを入力してください'
+        ]);
+    }
+
+    public function test_admin_login_validation_error_when_credentials_are_wrong()
+    {
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('adminpass123'),
+            'is_admin' => true,
+        ]);
+
+        $data = [
+            'email' => 'wrong_admin@example.com',
+            'password' => 'adminpass123',
+        ];
+
+        $response = $this->post('/admin/login', $data);
+
+        $response->assertSessionHasErrors([
+            'email' => 'ログイン情報が登録されていません'
+        ]);
+    }
+}
